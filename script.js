316836059fe3087c8bfb3ada5ec69100a5f969a7
Bazou76 <mathis.pain76@gmail.com>
2025-02-11 16:33:56 +0100

    mise a jour

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ burgerMenuButton.onclick = function () {
   //cacher contact quand le menu est ouvert
   acceuil.classList.toggle("afficher-contact");
 };
+
+const arrowButton = document.querySelectorAll(".arrow1");
+console.log(arrowButton);
+const projet1 = document.querySelector(".projet1");
+const projet2 = document.querySelector(".projet2");
+
+arrowButton[0].onclick = function () {
+  //changer le projet
+  projet1.classList.toggle("afficherprojet1");
+  projet2.classList.toggle("afficherprojet2");
+};
+arrowButton[1].onclick = function () {
+  //changer le projet
+  projet1.classList.toggle("afficherprojet1");
+  projet2.classList.toggle("afficherprojet2");
+};
